c7ec5377da09b7dc6e3076bc63769a29a7f3e73d
ErickFSilva <erick_ferreira@outlook.com>
2022-12-19 16:46:45 -0300

    curso_php_oo_19-12-22(tarde)

---
+++ aula014_heranca_visualizar_registro_db/User.php
@@ -0,0 +1,48 @@
+<?php
+
+class User extends Conn
+{
+    // Atributos
+    public object $conn;
+    public array $formData;
+
+    // Métodos
+    public function list(): array
+    {
+        $this->conn = $this->connectDb();
+
+        $query_users = "select id, name, email from users order by id desc limit 40";
+
+        $result_users = $this->conn->prepare($query_users);
+        $result_users->execute();
+
+        $retorno = $result_users->fetchAll();
+
+        return $retorno;
+    }
+
+    public function create(): bool
+    {
+        // var_dump($this->formData);
+
+        $this->conn = $this->connectDb();
+
+        $query_user = "insert into users (name, email, created) values (:name, :email, now())";
+        
+        $add_user = $this->conn->prepare($query_user);
+        $add_user->bindParam(':name', $this->formData['name']);
+        $add_user->bindParam(':email', $this->formData['email']);
+        $add_user->execute();
+
+        // Verifica se foi cadastrado com sucesso!
+        if($add_user->rowCount())
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+
+}
---
+++ aula014_heranca_visualizar_registro_db/index.php
@@ -0,0 +1,71 @@
+<!-- Incorpora as classes na página -->
+<?php
+session_start();
+
+require "Conn.php";
+require "User.php";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OO</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</head>
+
+<body class="p-3">
+
+    <div class="container">
+        <div class="row">
+
+            <nav class="navbar navbar-expand navbar-dark bg-dark mb-4">
+                <ul class="navbar-nav">
+                    <li class="navbar-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Listar</a>
+                    </li>
+                    <li class="navbar-item">
+                        <a class="nav-link" href="Create.php">Cadastrar</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <h1 class="mb-4">Listar Usuário</h1>
+
+            <?php
+
+            if(isset($_SESSION['msg']))
+            {
+                echo $_SESSION['msg'];
+                // Destroi o conteúdo da variável global após a sua impressão
+                unset($_SESSION['msg']);
+            }
+
+            $listUser = new User();
+            $result_users = $listUser->list();
+
+            foreach($result_users as $row_user)
+            {
+                extract($row_user);
+
+                echo "ID: $id <br>";
+                echo "Nome: $name <br>";
+                echo "E-mail: $email <br>";
+                echo "<a href='view.php?id=$id' class='nav-link text-primary'>Visualizar</a><br>";
+                echo "<hr>";
+            }
+
+            ?>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
